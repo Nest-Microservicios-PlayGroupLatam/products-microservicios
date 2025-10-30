11a2aba84e9970336609f2cd73cbd86c22b30589
EnriquePlayGroupLatam <enrique@playgrouplatam.com>
2025-10-30 12:36:50 -0500

    32.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -25,7 +25,14 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
+
+
+
+    // return {
+    //   id, updateProductDto
+    // }
+
     return this.productsService.update(+id, updateProductDto);
   }
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '../../generated/prisma/client';
@@ -40,12 +40,29 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
       }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    // return `This action returns a #${id} product`;
+    const product = await this.product.findFirst({
+      where: {id}
+    });
+
+    if( !product ){
+      throw new NotFoundException(`Product with id #${ id } not found`);
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    await this.findOne(id)
+
+    return this.product.update({
+      where: { id },
+      data: updateProductDto,
+    })
+
+    // return `This action updates a #${id} product`;
   }
 
   remove(id: number) {
